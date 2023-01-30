682fcef53fdd5326de6de4beb92be9c436fefdd3
tarana <beshirova@hotmail.com>
2023-01-30 16:31:57 +0300

    task2

---
+++ task2.java
@@ -0,0 +1,25 @@
+package seminar2;
+
+
+
+public class task2 {
+    public static void main(String[] args) {
+        String s = "babab";
+        if (palindrom(s)){
+            System.out.println("\nВведенная строка является палиндромом!\n");
+        }
+        else {System.out.println("\nВведенная строка НЕ является палиндромом!\n");}
+    }
+
+    public static boolean palindrom(String s){
+        String temp = "";
+        int l = s.length();
+        for (int i = 0; i < l; i++){
+            temp = temp + s.charAt(l-i-1);
+        }
+        if (temp.equals(s)){
+            return true;
+        }
+        else {return false;}
+    }
+}
